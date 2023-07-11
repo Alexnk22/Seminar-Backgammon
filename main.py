1676888d3976cbd2f151f7f95f8d3512fbe1a4f8
Alexnk22 <alexk4433@gmail.com>
2023-07-11 16:06:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ canvas = Canvas(root,height=550, width=550)
 canvas.pack(side=TOP,fill=BOTH,expand=YES)
 
 
-spielfeld1 = [(2,0,0,0,0,0   ,0,0,0,0,0,5       ,0,0,0,0,3,0,   5,0,0,0,0,0)]    #withe 
-spielfeld1 = [(2,0,0,0,0,0   ,0,0,0,0,0,5       ,0,0,0,0,3,0,   5,0,0,0,0,0)]    #black
+spielfeld1 = (2,0,0,0,0,0   ,0,0,0,0,0,5       ,0,0,0,0,3,0,   5,0,0,0,0,0)    #withe 
+spielfeld2 = (2,0,0,0,0,0   ,0,0,0,0,0,5       ,0,0,0,0,3,0,   5,0,0,0,0,0)    #black
 
-#zum Vergrößern(fehler mit dem Würfel knopf)
+
 def Ratios (event=NONE):
     canvas.delete("all")
     
@@ -23,23 +23,30 @@ def Ratios (event=NONE):
         Ratio = root.winfo_height()/800
         
     Feld(Ratio)
+    Figuren(Ratio)
 
 #Erstellt das Feld 
 def Feld(Ratio):
     
     for u in range (2):
-        canvas.create_rectangle((75+u*332.5)*Ratio,75*Ratio,(392.5+u*332.5)*Ratio,650*Ratio)
-        canvas.create_rectangle(75*Ratio,75*Ratio,(725)*Ratio,650*Ratio,width=1.5)
+        canvas.create_rectangle((75+u*332.5)*Ratio,75*Ratio,(392.5+u*332.5)*Ratio,650*Ratio,fill="goldenrod",width=2)
+        canvas.create_rectangle(75*Ratio,75*Ratio,(725)*Ratio,650*Ratio,width=4)
 
     for u in range (2):
         for i in range (3):
-            canvas.create_polygon((i*106+75+u*332.5)*Ratio, 75*Ratio, (i*106+100+u*332.5)*Ratio, 325*Ratio, (i*106+125+u*332.5)*Ratio, 75*Ratio,fill="white",outline="black",)
-            canvas.create_polygon((i*106+127.5+u*332.5)*Ratio, 75*Ratio, (i*106+152.5+u*332.5)*Ratio, 325*Ratio, (i*106+177.5+u*332.5)*Ratio, 75*Ratio)
+            canvas.create_polygon((i*106+75+u*333)*Ratio, 75*Ratio, (i*106+100+u*333)*Ratio, 325*Ratio, (i*106+125+u*333)*Ratio, 75*Ratio,fill="red",outline="black",)
+            canvas.create_polygon((i*106+127.5+u*333)*Ratio, 75*Ratio, (i*106+152.5+u*333)*Ratio, 325*Ratio, (i*106+177.5+u*333)*Ratio, 75*Ratio)
 
-            canvas.create_polygon((i*106+75+u*332.5)*Ratio, 650*Ratio, (i*106+100+u*332.5)*Ratio, 400*Ratio, (i*106+125+u*332.5)*Ratio, 650*Ratio)
-            canvas.create_polygon((i*106+127.5+u*332.5)*Ratio, 650*Ratio, (i*106+152.5+u*332.5)*Ratio, 400*Ratio, (i*106+177.5+u*332.5)*Ratio, 650*Ratio,fill="white",outline="black")
+            canvas.create_polygon((i*106+75+u*333)*Ratio, 650*Ratio, (i*106+100+u*333)*Ratio, 400*Ratio, (i*106+125+u*333)*Ratio, 650*Ratio)
+            canvas.create_polygon((i*106+127.5+u*333)*Ratio, 650*Ratio, (i*106+152.5+u*333)*Ratio, 400*Ratio, (i*106+177.5+u*333)*Ratio, 650*Ratio,fill="red",outline="black")
 
 
+def Figuren(Ratio):
+
+    global spielfeld1
+
+    for i in range(int(spielfeld1[0])):
+        canvas.create_oval(673*Ratio,(600-(i)*50)*Ratio,723*Ratio,(650-(i)*50)*Ratio,fill="white")
 
 root.bind("<Configure>", Ratios)
 root.mainloop()
