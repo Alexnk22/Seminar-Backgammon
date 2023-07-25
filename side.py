418633071224b33317d9bce4b763d2b1d5093a75
Alexnk22 <131484527+Alexnk22@users.noreply.github.com>
2023-07-25 17:14:44 +0200

    h

--- side.py
+++ side.py
@@ -1,6 +1,12 @@
 from tkinter import*
 root = Tk()
 
+canvas = Canvas(root,height=300, width=400)
+canvas.pack(side=TOP,fill=BOTH,expand=YES)
 
+def open():
+    root.destroy()
+
+btn = Button(root,text="Play",command=open).pack()
 
 root.mainloop()
