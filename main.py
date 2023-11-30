fcec629f91076dd268cd486e6c59dc04abfe6aba
Alexnk22 <alexk4433@gmail.com>
2023-11-30 14:36:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ spielfeld1 = [2,0,0,0,0,0   ,0,0,0,0,0,5       ,0,0,0,0,3,0,   5,0,0,0,0,0]
 #            
 #                       0 1 2        3 4 5 6 7 8    9 0 1 2 3 4
 
-
 def Ratios (event=NONE):
     global White_Cap_Piece,Red_Cap_Piece
     if regelcounter == 0:
